ca8761726718e9dc0507fc6bdbe976db241c3902
OmarElprolosy66 <omarelsayed6600@gmail.com>
2024-01-29 14:50:24 +0200

    added portfolios' images to the portfolio section in index.html

--- dashboard/lib/libportfolio.php
+++ dashboard/lib/libportfolio.php
@@ -1,23 +1,45 @@
 <?php
 
-function insert_portfdata($user_id, $img, $desc) {
+function insert_portfdata($user_id, $img, $desc)
+{
     $db = mysqli_connect("127.0.0.1", "root", "", "proone");
-    
+
     $query = "INSERT INTO `portfolio` (user_id, image, description) VALUES ('$user_id', '$img', '$desc')";
     mysqli_query($db, $query);
 
     return (bool)mysqli_affected_rows($db);
 }
 
-function get_portfolios() {
+function get_portfolios()
+{
     $db = mysqli_connect("127.0.0.1", "root", "", "proone");
+
+    // Check connection
+    if (!$db) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
     $storage = [];
     $query = "SELECT * FROM `portfolio`";
-    
-    while ($data = mysqli_fetch_assoc(mysqli_query($db, $query))) {
-        $storage[] = $data;
+
+    // Execute the query
+    $result = mysqli_query($db, $query);
+
+    if ($result) {
+        // Fetch the results
+        while ($data = mysqli_fetch_assoc($result)) {
+            $storage[] = $data;
+        }
+
+        // Free the result set
+        mysqli_free_result($result);
+    } else {
+        // Handle query error
+        echo "Error: " . mysqli_error($db);
     }
+
+    // Close the connection
+    mysqli_close($db);
 
     return $storage;
 }
