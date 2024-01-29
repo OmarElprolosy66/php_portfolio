e464ec094ab6609fa9ac94afc638a51f678581c6
OmarElprolosy66 <omarelsayed6600@gmail.com>
2024-01-29 16:36:52 +0200

    added portfolio table in the dashboard to give admin control on the  added portfolios and see who added each one

--- dashboard/lib/libportfolio.php
+++ dashboard/lib/libportfolio.php
@@ -43,3 +43,38 @@ function get_portfolios()
 
     return $storage;
 }
+
+
+function get_portfolios_byname()
+{
+    $db = mysqli_connect("127.0.0.1", "root", "", "proone");
+
+    // Check connection
+    if (!$db) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $storage = [];
+    $query = "SELECT * FROM `user_portfolio`";
+
+    // Execute the query
+    $result = mysqli_query($db, $query);
+
+    if ($result) {
+        // Fetch the results
+        while ($data = mysqli_fetch_assoc($result)) {
+            $storage[] = $data;
+        }
+
+        // Free the result set
+        mysqli_free_result($result);
+    } else {
+        // Handle query error
+        echo "Error: " . mysqli_error($db);
+    }
+
+    // Close the connection
+    mysqli_close($db);
+
+    return $storage;
+}
